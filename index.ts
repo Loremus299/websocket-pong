b1a882d155e7f869f4459625bb8bf28d8e496b9a
loremus <loremus@proton.me>
2026-09-05 15:27:05 +0530

    message

--- index.ts
+++ index.ts
@@ -14,43 +14,47 @@ const wss = new WebSocketServer({ server });
 wss.on("connection", (ws, req) => {
   console.log("client connected from", req.socket.remoteAddress);
   ws.on("message", (data) => {
-    const input = JSON.parse(data.toString());
     const log = new Logger();
-    log.info({ layer: "websocket message" });
-    log.info({ body: input });
+    try {
+      log.info({ data });
+      log.info({ layer: "websocket message" });
+      const input = JSON.parse(data.toString());
 
-    log.trace({ op: "body validation" });
-    const schema = z.object({
-      session: z.string().min(1),
-      user: z.string().min(1),
-      type: z.enum(["add", "sub"]),
-    });
-    const values = schema.safeParse(input);
-    if (!values.success) {
-      return ws.send(
-        JSON.stringify({
-          error:
-            "Invalid body format, requires session, user, type as strings for player1 and player2",
-          log: log.id,
-        }),
-      );
-    }
-    const { session, user, type } = values.data;
+      log.trace({ op: "body validation" });
+      const schema = z.object({
+        session: z.string().min(1),
+        user: z.string().min(1),
+        type: z.enum(["add", "sub"]),
+      });
+      const values = schema.safeParse(input);
+      if (!values.success) {
+        return ws.send(
+          JSON.stringify({
+            error:
+              "Invalid body format, requires session, user, type as strings for player1 and player2",
+            log: log.id,
+          }),
+        );
+      }
+      const { session, user, type } = values.data;
 
-    log.trace({ op: "resolve session" });
-    const res = currentSessions.resolve(session, log);
-    if (res.isError()) {
-      return ws.send(
-        JSON.stringify({
-          error: "session not resolved",
-          log: log.id,
-        }),
-      );
-    }
-    if (res.isOk()) {
-      log.trace({ op: "update position" });
-      const activeSession = res.data;
-      activeSession.updatePos({ id: user, type, log });
+      log.trace({ op: "resolve session" });
+      const res = currentSessions.resolve(session, log);
+      if (res.isError()) {
+        return ws.send(
+          JSON.stringify({
+            error: "session not resolved",
+            log: log.id,
+          }),
+        );
+      }
+      if (res.isOk()) {
+        log.trace({ op: "update position" });
+        const activeSession = res.data;
+        activeSession.updatePos({ id: user, type, log });
+      }
+    } finally {
+      log.dump();
     }
   });
   ws.on("close", () => console.log("client disconnected"));
