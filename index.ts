804d114f158a792cc3d2e365269a85564f863e57
loremus <loremus@proton.me>
2026-09-05 11:40:03 +0530

    basic setup

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import express, { type Express, type Request, type Response } from "express";
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("HELLO FROM THE SERVER SIDE!");
+});
+
+app.listen(3000);
