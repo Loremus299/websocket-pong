2f39bbbac18e40d689f3463b5e7068888d1349d5
loremus <loremus@proton.me>
2026-09-05 17:37:16 +0530

    current sessions

--- classes/current-sessions.ts
+++ classes/current-sessions.ts
@@ -9,20 +9,32 @@ class CurrentSessions {
     this.sessions = [];
   }
 
-  public add(session: GameSession) {
+  public add(session: GameSession, log: Logger) {
+    log.trace({ layer: "add game session to list" });
+    log.debug({ session: session.id });
     this.sessions.push(session);
   }
 
   public resolve(id: string, log: Logger): Result<GameSession, Err> {
+    log.trace({ layer: "resolve session by id" });
+    log.debug({ session: id });
     const session = this.sessions.find((session) => session.id === id);
     if (session) {
+      log.info({ session: session });
       return Result.ok(session);
     } else {
+      log.error({ error: "session not found" });
       return Result.error({
         status: 404,
         info: "Session with the provided id doesn't exist",
       });
     }
+  }
+
+  public remove(id: string, log: Logger) {
+    log.trace({ layer: "remove game session from list" });
+    log.debug({ session: id });
+    this.sessions.filter((i) => i.id !== id);
   }
 }
 
